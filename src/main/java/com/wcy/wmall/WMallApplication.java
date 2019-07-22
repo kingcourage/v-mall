f0f857498982aab1b54a5ce52a568d2f6fdc3645
kingcourage <996817807@qq.com>
2019-07-22 15:48:34 +0800

    init commit

---
+++ src/main/java/com/wcy/wmall/WMallApplication.java
@@ -0,0 +1,13 @@
+package com.wcy.wmall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WMallApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WMallApplication.class, args);
+    }
+
+}
