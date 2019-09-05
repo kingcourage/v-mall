4f843ce13b37e7d044b873630aee064d355c0a0f
wcy <996817807@qq.com>
2019-09-05 14:15:19 +0800

    user

---
+++ mall-admin/src/main/java/com/wcy/wmall/controller/UmsAdminController.java
@@ -0,0 +1,18 @@
+package com.wcy.wmall.controller;
+
+import io.swagger.annotations.Api;
+import org.springframework.stereotype.Controller;
+
+/**
+ * @ClassName UmsAdminController
+ * @Description TODO
+ * @Author wcy
+ * @Date 2019-08-29 14:34
+ * @Version 1.0
+ **/
+@Controller
+@Api(tags = "UmsAdminController",description = "后台用户管理")
+public class UmsAdminController {
+
+
+}
---
+++ mall-admin/src/main/java/com/wcy/wmall/service/UmsAdminService.java
@@ -0,0 +1,13 @@
+package com.wcy.wmall.service;
+
+/**
+ * @ClassName UmsAdminService
+ * @Description TODO
+ * @Author wcy
+ * @Date 2019-08-29 14:36
+ * @Version 1.0
+ **/
+public class UmsAdminService {
+
+
+}
